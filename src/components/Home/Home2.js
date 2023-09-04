d71eeaed398d0614de6860ecd75deb7a343972aa
Aman Amin <94545891+Amanamin2k6@users.noreply.github.com>
2023-09-04 00:53:39 -0500

    Update Home2.js

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -18,7 +18,7 @@ function Home2() {
               LET ME <span className="purple"> INTRODUCE </span> MYSELF
             </h1>
             <p className="home-about-body">
-              For as long as i can remember, I have loved technology.
+              For as long as I can remember, I have loved technology.
               <br />
               <br />I am fluent in classics like
               <i>
@@ -26,12 +26,12 @@ function Home2() {
               </i>
               <br />
               <br />
-              My field of Interest's are building new &nbsp;
+              My field of Interest is building new &nbsp;
               <i>
                 <b className="purple">Web Technologies and Products </b> and
                 also in areas related to{" "}
                 <b className="purple">
-                  Deep Learning and Natural Launguage Processing.
+                  Deep Learning and Natural Language Processing.
                 </b>
               </i>
               <br />
