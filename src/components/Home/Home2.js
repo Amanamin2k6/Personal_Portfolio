24aafb01d2a817754ce1bcb5005542c179f21318
Aman Amin <94545891+Amanamin2k6@users.noreply.github.com>
2023-09-04 00:31:03 -0500

    Update Home2.js

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -57,7 +57,7 @@ function Home2() {
             <ul className="home-about-social-links">
               <li className="social-icons">
                 <a
-                  href="https://github.com/HUHMAN"
+                  href="https://github.com/Amanamin2k6"
                   target="_blank"
                   rel="noreferrer"
                   className="icon-colour  home-social-icons"
