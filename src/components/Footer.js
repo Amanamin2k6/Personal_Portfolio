f7249fdd755aab01e8202cf8ac9e1b03efa03f21
Aman Amin <94545891+Amanamin2k6@users.noreply.github.com>
2023-09-04 00:31:55 -0500

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -22,7 +22,7 @@ function Footer() {
           <ul className="footer-icons">
             <li className="social-icons">
               <a
-                href="https://github.com/HUHMAN"
+                href="https://github.com/Amanamin2k6"
                 style={{ color: "white" }}
                 target="_blank" 
                 rel="noopener noreferrer"
